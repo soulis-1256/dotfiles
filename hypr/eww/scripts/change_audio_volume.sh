fc9593594f7ca63f51aeb076447df2f923febf20
Anastasios Andreou <118274635+soulis-1256@users.noreply.github.com>
2024-04-12 22:37:16 +0300

    update eww

--- hypr/eww/scripts/change_audio_volume.sh
+++ hypr/eww/scripts/change_audio_volume.sh
@@ -1,7 +1,61 @@
-#/bin/bash
+#!/bin/bash
 
-if test $1 == "up"; then
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.01+ --limit 1.0
-elif test $1 == "down"; then
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.01-
+# Check if exactly two arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 [sink|source] [up|down|toggle]" >&2
+    exit 1
+fi
+
+if [[ $2 != "toggle" ]]; then
+    # Determine the audio device based on the first argument
+    case "$1" in
+        sink)
+            audio_device="@DEFAULT_AUDIO_SINK@"
+            ;;
+        source)
+            audio_device="@DEFAULT_AUDIO_SOURCE@"
+            ;;
+        *)
+            echo "Usage: $0 [sink|source] [up|down|toggle]" >&2
+            exit 1
+            ;;
+    esac
+
+    # Determine the volume adjustment based on the second argument
+    case "$2" in
+        up)
+            adjustment="0.01+"
+            ;;
+        down)
+            adjustment="0.01-"
+            ;;
+        *)
+            echo "Usage: $0 [sink|source] [up|down|toggle]" >&2
+            exit 1
+            ;;
+    esac
+
+    # Adjust the volume
+    wpctl set-volume "$audio_device" "$adjustment" --limit 1.0
+else
+    if [[ $1 == "sink" ]]; then
+        sink_status=$(pactl get-sink-mute @DEFAULT_SINK@)
+        if [[ $sink_status == "Mute: no" ]]; then
+            eww update sink_muted=true
+        else
+            eww update sink_muted=false
+        fi
+        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+    elif [[ $1 == "source" ]]; then
+        source_status=$(pactl get-source-mute @DEFAULT_SOURCE@)
+        if [[ $source_status == "Mute: no" ]]; then
+            eww update source_muted=true
+        else
+            eww update source_muted=false
+        fi
+        wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+    else
+        echo "Usage: $0 [sink|source] [up|down|toggle]" >&2
+        exit 1
+    fi
 fi
--- hypr/eww/scripts/trace_audio_volume.sh
+++ hypr/eww/scripts/trace_audio_volume.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
+# Check if exactly one argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 [sink|source]" >&2
+    exit 1
+fi
+
+# Determine the audio device based on the argument and initialize mute status
+case "$1" in
+    sink)
+        audio_device="@DEFAULT_AUDIO_SINK@"
+        sink_status=$(pactl get-sink-mute @DEFAULT_SINK@)
+        if [[ $sink_status == "Mute: no" ]]; then
+            eww update sink_muted=false
+        else
+            eww update sink_muted=true
+        fi
+        ;;
+    source)
+        audio_device="@DEFAULT_AUDIO_SOURCE@"
+        if [[ $source_status == "Mute: no" ]]; then
+            eww update source_muted=false
+        else
+            eww update source_muted=true
+        fi
+
+        ;;
+    *)
+        echo "Usage: $0 [sink|source]" >&2
+        exit 1
+        ;;
+esac
+
 # Initialize previous volume
 prev_volume=""
 
 while true; do
     # Get current volume
-    current_volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk -F': ' '{print $2 * 100}')
+    current_volume=$(wpctl get-volume "$audio_device" | awk -F': ' '{print $2 * 100}')
 
     # Check if the current volume is different from the previous one
     if [ "$current_volume" != "$prev_volume" ]; then
